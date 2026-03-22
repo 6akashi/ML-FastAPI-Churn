abc429d10651fd25c15bf408095af5bba04305d4
6akashi <semen.yby@gmail.com>
2026-03-22 21:17:48 +0300

    added feature and train pydantic models and test predict endpoint

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from models.FeatureVectorChurn import FeatureVectorChurn
+
 app = FastAPI()
 
 @app.get("/")
 def say_hello():
       return {"message": "ml churn service is running"}
+
+@app.post("/predict")
+def predict(features: FeatureVectorChurn):
+      return features
